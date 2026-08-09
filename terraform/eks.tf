2f6e19991cbfa9436c8a87456fb1a88d6d4eb612
davidwallacedot <davidwallacedot@yahoo.com>
2026-08-09 15:00:27 -0500

    adding access_entries

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,7 +5,27 @@ module "eks" {
   name               = "techchallenge2-eks"
   kubernetes_version = "1.33"
 
-  endpoint_public_access = true
+  endpoint_public_access  = true
+  endpoint_private_access = true
+
+  authentication_mode = "API_AND_CONFIG_MAP"
+
+  access_entries = {
+    terraform_admin = {
+      principal_arn = "arn:aws:iam::258083582548:user/Kubernetes-Admin"
+
+      policy_associations = {
+        cluster_admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+
+  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
